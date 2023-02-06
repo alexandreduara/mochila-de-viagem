2c561b24d480bb0721f767236d29ab7316487a2c
alexandreduara <alexandreduara@gmail.com>
2023-02-06 14:49:43 -0300

    Terminando o projeto mochila de viagem

--- js/main.js
+++ js/main.js
@@ -24,8 +24,10 @@ form.addEventListener("submit", (evento) => {
         itemAtual.id = existe.id
 
         atualizaElemento(itemAtual)
+
+        itens[itens.findIndex(elemento => elemento.id === existe.id)] = itemAtual
     } else {
-        itemAtual.id = itens.length
+        itemAtual.id = itens[itens.lenght -1] ? (itens[itens.length-1]).id + 1 : 0;
 
         criaElemento(itemAtual);
 
@@ -54,6 +56,8 @@ function criaElemento(item) {
     novoItem.appendChild(numeroItem);
     novoItem.innerHTML += item.nome;
 
+    novoItem.appendChild(botaoDeleta(item.id))
+
     lista.appendChild(novoItem);
 
 
@@ -62,4 +66,23 @@ function criaElemento(item) {
 
 function atualizaElemento (item){
     document.querySelector("[data-id='"+item.id+"']").innerHTML = item.quantidade
+}
+
+function botaoDeleta(id){
+    const elementoBotao = document.createElement("button");
+    elementoBotao.innerText = "X"
+
+    elementoBotao.addEventListener("click", function() {
+        deletaElemento(this.parentNode, id)
+    })
+
+    return elementoBotao
+}
+
+function deletaElemento(tag, id) {
+    tag.remove();
+
+    itens.splice(itens.findIndex(elemento => elemento.id === id) ,1);
+
+    localStorage.setItem("itens", JSON.stringify(itens));
 }
